c903839b246253b9121465215ae2c6fd5fb6bd40
GraphTree <graphnodesed@gmail.com>
2022-09-06 23:29:43 +0900

    framer test

---
+++ src/atoms.tsx
@@ -0,0 +1,19 @@
+import {atom, selector} from 'recoil';
+
+export const minuteState = atom({
+    key: 'minutes',
+    default: 0
+});
+
+export const hourSelector = selector({
+    key: 'hourState',
+    get : ({get}) => {
+        const minute = get(minuteState);
+        return minute/60;
+    },
+    set : ({set}, newValue) => {
+        const minutes= Number(newValue)*60;
+        set(minuteState, minutes);
+
+    }
+});
---
+++ src/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { RecoilRoot } from 'recoil';
+import {darkTheme} from './theme/theme';
+import {ThemeProvider} from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: lightblue;
+  }
+`
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  // <React.StrictMode>
+  <ThemeProvider theme={darkTheme}>
+    
+    <RecoilRoot>  
+      <GlobalStyle/>
+      <App />
+    </RecoilRoot>  
+  </ThemeProvider>
+  // </React.StrictMode>
+);
+
